326255b9dc15026ad155a0f50dec7f20ad381265
joelisreal <joel.igberase@gmail.com>
2019-04-18 12:47:15 -0600

    Added podfile, imported fidgets module, added code to ruin on both led's when button clicked

--- Phidgets_TwoLEDs_Test/ViewController.swift
+++ Phidgets_TwoLEDs_Test/ViewController.swift
@@ -7,12 +7,87 @@
 //
 
 import UIKit
+import Phidget22Swift
 
 class ViewController: UIViewController {
+    
+    let button = DigitalInput()
+    let ledArray = [DigitalOutput(), DigitalOutput()]
 
+    func attach_handler(sender: Phidget){
+        do{
+            let hubPort = try sender.getHubPort()
+            if(hubPort == 0){
+                print("LED 0 Attached")
+            }
+            else if (hubPort == 1){
+                print("LED 1 Attached")
+            }
+            else{
+                print("Button Attached")
+            }
+        } catch let err as PhidgetError{
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
+    func state_change(sender:DigitalInput, state:Bool){
+        do{
+            if(state == true){
+                print("Button Pressed")
+                try ledArray[0].setState(true)
+                try ledArray[1].setState(true)
+            }
+            else{
+                print("Button Not Pressed")
+                try ledArray[0].setState(false)
+                try ledArray[1].setState(false)
+            }
+        } catch let err as PhidgetError{
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        do{
+            //enable server discovery
+            try Net.enableServerDiscovery(serverType: .deviceRemote)
+            
+            //address digital input
+            try button.setDeviceSerialNumber(528025)
+            try button.setHubPort(2)
+            try button.setIsHubPortDevice(true)
+            
+            //add event handler
+            let _ = button.stateChange.addHandler(state_change)
+            
+            //add attach handler
+            let _ = button.attach.addHandler(attach_handler)
+            
+            //open
+            try button.open()
+            
+            //address, add handler, open digital outputs
+            for i in 0..<ledArray.count{
+                try ledArray[i].setDeviceSerialNumber(528025)
+                try ledArray[i].setHubPort(i)
+                try ledArray[i].setIsHubPortDevice(true)
+                let _ = ledArray[i].attach.addHandler(attach_handler)
+                try ledArray[i].open()
+            }
+            
+        } catch let err as PhidgetError {
+            print("Phidget Error " + err.description)
+        } catch {
+            //catch other errors here
+        }
     }
 
 
---
+++ Pods/Target Support Files/Pods-Phidgets_TwoLEDs_Test/Pods-Phidgets_TwoLEDs_Test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Phidgets_TwoLEDs_Test : NSObject
+@end
+@implementation PodsDummy_Pods_Phidgets_TwoLEDs_Test
+@end
